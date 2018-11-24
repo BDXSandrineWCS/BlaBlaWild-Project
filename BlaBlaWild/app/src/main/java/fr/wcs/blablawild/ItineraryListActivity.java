130df2c448c2d14354d8998f8afbb27b4d9ca33d
sandrine <poupardsandrine@gmail.com>
2018-11-24 17:20:57 +0100

    First step of Afficher des listes d'objets grâce à un adapter Quest get existing blablawild project

---
+++ BlaBlaWild/app/src/main/java/fr/wcs/blablawild/ItineraryListActivity.java
@@ -0,0 +1,24 @@
+package fr.wcs.blablawild;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItineraryListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_itinerary_list);
+
+        // Get the Intent that started this activity (from ItinerarySearchActivity) and extract the extra (departure destination and dateSetListener)
+        Intent intent = getIntent();
+        String departure = intent.getStringExtra("EXTRA_DEPARTURE");
+        String destination = intent.getStringExtra("EXTRA_DESTINATION");
+        String date = intent.getStringExtra("EXTRA_DATE");
+        String title="";
+        if ((departure != null) && (destination!= null)) title = (departure + " >> " + destination);
+        this.setTitle(title);
+
+    }
+}
